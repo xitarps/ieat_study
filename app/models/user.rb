8ccf8f121f81044858a6da306fc35605efd04e89
xita Rps <xitarps@gmail.com>
2021-05-04 09:44:20 -0300

    Add restaurant to user and enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  has_one :restaurant
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: {admin: 0, restaurant: 1, customer: 2}
 end
