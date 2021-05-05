fbea8da54a9318dcbc4efc15a790959c606d30c9
xita Rps <xitarps@gmail.com>
2021-05-04 21:03:46 -0300

    Add order

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,22 @@
+class OrdersController < ApplicationController
+  def create
+    @order = Order.new(order_params)
+    @order.user_id = current_user.id
+    @order.status = :pending
+
+    byebug
+    if @order.save
+      flash[:notice] = "Pedido criado com sucesso!"
+    else
+      flash[:alert] = "Oops, something went wrong"
+    end
+    redirect_to root_path
+
+  end
+
+  private
+
+  def order_params
+    params.permit(:product_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'orders/create'
   get 'products/index'
   resources :restaurants
   resources :products, only: %i[index new create destroy]
+
+  resources :orders, only: %i[ create ]
   
   devise_for :users
   root 'home#index'
